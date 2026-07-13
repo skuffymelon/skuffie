f023dc9fb690986f11ee889c823dee8551395d85
skuffymelon <110454766+skuffymelon@users.noreply.github.com>
2026-07-13 04:53:57 +0200

    site 2
    mewo meow :3

--- scriptindex.js
+++ scriptindex.js
@@ -3,8 +3,34 @@ function loadtext()
     for (let index = 0; index < document.getElementsByClassName("letter").length; index++) {
         setTimeout(() =>
         {
-            var element = document.getElementsByClassName("letter")[index];
-            element.style.animation = "animfade 0.68s";
+            document.getElementsByClassName("letter")[index].style.animation = "animfade 0.68s";
         }, 150 * (index + 1));
     }
+    setTimeout(() => 
+    {
+        for (let index = 0; index < document.getElementsByClassName("letter").length; index++) {
+            document.getElementsByClassName("letter")[index].style.animation = "";
+        }
+    }, 1730);
+}
+
+var globalstring = "";
+
+function clickletter(element)
+{
+    globalstring += element.innerHTML;
+
+    element.style = "text-shadow: 4px 4px 4px purple;";
+
+    if (globalstring == "suffiks" || globalstring == "sufiks")
+    {
+        document.querySelector(".skuffieabout h1").innerHTML = "haii im emma 🏳️‍⚧️ :3";
+        document.title = "skuffie/emma"
+        document.querySelector(".skuffieabout p").innerHTML = "tranz/pan</br>she/her x3";
+    }
+
+    setTimeout(() => {
+        globalstring = "";
+        element.style = "text-shadow: none;";
+    }, 5000);
 }
